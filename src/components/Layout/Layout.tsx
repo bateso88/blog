238274153919ef6f6648571e2a1e333e24128230
y <y>
2022-10-14 13:09:31 +0100

    [main] Add basic layout

---
+++ src/components/Layout/Layout.tsx
@@ -0,0 +1,14 @@
+import Header from '@Components/Header'
+import React, { ReactNode } from 'react'
+
+type Props = {
+  children: ReactNode;
+}
+
+const Layout = ({children}: Props) => {
+  return (
+    <><Header/>{children}</>
+  )
+}
+
+export default Layout
