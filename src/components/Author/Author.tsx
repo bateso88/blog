9441207d99680c44a701b05a426de15a1c00e767
y <y>
2023-04-14 17:09:49 +0100

    [main] Add author section

--- src/components/Author/Author.tsx
+++ src/components/Author/Author.tsx
@@ -1,7 +1,27 @@
-const Author = () => {
+import { Author } from "@Types/posts";
+import Image from "next/image";
+
+type Props = {
+  author: Author;
+};
+
+const Author = ({ author }: Props) => {
   return (
-    <div>
-      <h1>Author</h1>
+    <div className="mb-8 relative rounded-lg bg-black bg-opacity-20">
+      <div className="flex flex-row">
+        <Image
+          alt={author.name}
+          unoptimized
+          height="80"
+          width="180"
+          className="align-middle rounded"
+          src={author.photo.url}
+        />
+      <div className="m-auto text-center">
+      <h3 className="text-white text-xl font-bold">{author.name}</h3>
+      <p className="text-white text-lg">{author.bio}</p>
+      </div>
+      </div>
     </div>
   );
 };
